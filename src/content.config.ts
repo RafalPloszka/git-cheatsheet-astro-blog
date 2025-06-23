0ba12ce479d0b37f205df7e3fcb827031b058ecf
RafalPloszka <rafalploszka@live.com>
2025-06-23 10:12:12 +0200

    feat: add questions collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,34 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const cheatsheet = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    order: z.number(),
+  }),
 });
 
-export const collections = { blog };
+const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const questions = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    level: z.enum(["basic", "intermediate", "advanced"]),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { cheatsheet, blog, questions };
